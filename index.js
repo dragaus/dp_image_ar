210740b71a40c7aa2d918989948d71a25d56ae99
Francisco Cuauhtemoc Castañeda Rovira <31245060+dragaus@users.noreply.github.com>
2022-05-06 10:25:31 -0500

    Play audio on marker

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const loading = document.querySelector('#loading');
+
 let numberOfNftsToLoad = 2;
+
+const audio = new Audio('test.mp3');
+// console.log(audio);
+// audio.play();
 
 window.addEventListener("arjs-nft-loaded", (event) => {
     numberOfNftsToLoad--;
     if(numberOfNftsToLoad <= 0){
         loading.style.display = 'none';
     }
+});
+
+window.addEventListener('markerFound', (event) => {
+    audio.play();
 });
